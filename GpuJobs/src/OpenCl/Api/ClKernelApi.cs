c61440887dd577b1dc57595a3baa0b8bd3ebc0a8
firedef <firedef2019@gmail.com>
2021-10-08 12:14:09 +0300

    test 2

--- GpuJobs/src/OpenCl/Api/ClKernelApi.cs
+++ GpuJobs/src/OpenCl/Api/ClKernelApi.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 namespace GpuJobs.OpenCl.Api {
 	public static class ClKernelApi {
 		[DllImport("OpenCL", EntryPoint = "clCreateKernel")]
-		public static extern IntPtr CreateKernel_(
+		public static extern IntPtr ClCreateKernel(
 			IntPtr program,
 			[MarshalAs(UnmanagedType.LPStr)] string kernelName,
 			[Out] [MarshalAs(UnmanagedType.I4)] out int errorCode
